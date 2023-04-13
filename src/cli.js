c4ad3f7caa0c79baf0f9f039e28f1e192999f5e5
ivan <ivancherak@mail.ru>
2023-04-13 12:01:14 +0300

    games

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const test = () => {
   console.log('Welcome to the Brain Games!');
-  const username = readlineSync.question(' May I have your name? ');
+  const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!`);
 };
 export default test;
